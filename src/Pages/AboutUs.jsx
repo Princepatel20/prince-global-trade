812cdedd80e31dcce3564fb55969a3956ff2aca3
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-03-11 21:22:53 +0530

    Add files via upload

---
+++ src/Pages/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const aboutus = () => {
+  return (
+    <div>
+      aboutus
+    </div>
+  )
+}
+
+export default aboutus
---
+++ src/Pages/OurTeam.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurTeam = () => {
+  return (
+    <div>
+      ourteam
+    </div>
+  )
+}
+
+export default OurTeam
