6d5ed461766f8bf9e480fc0fda73dd0062eae394
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-03-11 22:23:22 +0530

    Update TeamComponent.jsx

--- src/Components/TeamComponent/TeamComponent.jsx
+++ src/Components/TeamComponent/TeamComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './TeamComponent.css';
+import "./TeamComponent.css";
 import Team from "../Assets/Team.jpeg";
 
 const TeamComponent = () => {
@@ -7,9 +7,19 @@ const TeamComponent = () => {
     <div>
       <div className="team">
         <img src={Team} alt="Team" />
-        <div className="team-title">
-          <h2>Embark on a Transformative Journey</h2>
-        </div>
+      </div>
+      <div className="team-title">
+        <h2>Embark on a Transformative Journey</h2>
+      </div>
+      <div className="team-summary">
+        <p>
+          Prince Global Trade is an innovative and forward-thinking company that
+          goes beyond conventional solutions. They prioritize understanding and
+          meeting the unique needs of their customers, fostering long-lasting
+          partnerships. With a commitment to continuous improvement and a
+          culture of creativity, Prince Global Trade strives to provide
+          cutting-edge solutions that shape the current times.
+        </p>
       </div>
     </div>
   );
