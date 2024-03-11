38c3bf6d79dc396dc043ac9bd04582bb7ff06a1b
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-03-11 22:47:46 +0530

    Update Phrases.jsx

--- src/Components/Phrases/Phrases.jsx
+++ src/Components/Phrases/Phrases.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
-import './Phrases.css';
+import React from "react";
+import "./Phrases.css";
+
+import Bestsol from "../Assets/Bestsol.png";
+import Creativework from "../Assets/Creativework.png";
+import Design from "../Assets/Design.png";
 
 const Phrases = () => {
   return (
-    <div className="phrase-container">
-      <div className="phrase-item">
-        <i className="fas fa-palette"></i>
-        <span className="phrase">Avant-Garde Mavericks</span>
+    <div className="content-container">
+      <div className="phrase-container">
+        <div className="phrase-item">
+          <div className="icon-wrapper">
+            <img src={Bestsol} alt="Icon" className="phrase-icon" />
+          </div>
+          <span className="phrase">Avant-Garde Mavericks</span>
+        </div>
+        <div className="phrase-item">
+          <div className="icon-wrapper">
+            <img src={Creativework} alt="Icon" className="phrase-icon" />
+          </div>
+          <span className="phrase">Emblazoned Artistry</span>
+        </div>
+        <div className="phrase-item">
+          <div className="icon-wrapper">
+            <img src={Design} alt="Icon" className="phrase-icon" />
+          </div>
+          <span className="phrase">Transcendent Visualizations</span>
+        </div>
       </div>
-      <div className="phrase-item">
-        <i className="fas fa-brush"></i>
-        <span className="phrase">Emblazoned Artistry</span>
-      </div>
-      <div className="phrase-item">
-        <i className="fas fa-eye"></i>
-        <span className="phrase">Transcendent Visualizations</span>
+      <div className="summary-container">
+        <p className="summary-paragraph">
+          We espouse the conviction that every crucible is a harbinger for a
+          bespoke panacea. As a preeminent purveyor of tailored solutions, we
+          stand at the avant-garde of innovation, supplying a cornucopia of
+          offerings.
+        </p>
+        <p className="summary-paragraph">
+          Whether you are an artisan, wordsmith, visionary, or any creative
+          muse, Prince Global Trade unfurls portals to a realm of endless
+          prospects. From synergistic endeavors to freelance callings, unearth
+          the opus that resonates within your soul.
+        </p>
+        <p className="summary-paragraph">
+          Immerse your self in a communion of design virtuosos who share your
+          fervor for transcending the limits of opportunity. Synergize, change
+          epiphanies, and raise your craft alongside kindred spirits.
+        </p>
       </div>
     </div>
   );
