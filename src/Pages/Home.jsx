ffb40764beef0ac6db8371839cb60b7a055c4092
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-03-11 22:25:57 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Team from '../Components/TeamComponent/TeamComponent';
+import Phrases from '../Components/Phrases/Phrases';
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <Team/>
+      <Phrases/>
     </div>
   )
 }
