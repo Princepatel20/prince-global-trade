9bf60668876cfc5f4117a34dc420ee56b141d0c6
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-03-11 21:22:21 +0530

    Create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Team from '../Components/TeamComponent/TeamComponent';
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <Team/>
+    </div>
+  )
+}
+
+export default Home
